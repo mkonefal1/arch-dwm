ef83ee374a4a21d41cda8cf4bc543f7a1fb9504c
Archy <mkonefal1@gmail.com>
2023-11-06 20:59:59 +0100

    add all files

---
+++ src/dwmblocks/config.h
@@ -0,0 +1,12 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+
+/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/ 
+  {"",  "sb-volume", 1,  10},
+  {"",  "sb-clock",  60, 1},
+  {"",  "sb-internet", 5,  4},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
